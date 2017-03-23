9a18938bd4b04234e74a782f53420a1816bd1aa7
Peter Insley <peter.insley@gmail.com>
2017-03-23 04:18:56 -0400

    asfawer

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from flask import request
 
-from bokeh.embed import components
-from bokeh.plotting import figure
-from bokeh.resources import INLINE
-from bokeh.util.string import encode_utf8
+#from bokeh.embed import components
+#from bokeh.plotting import figure
+#from bokeh.resources import INLINE
+#from bokeh.util.string import encode_utf8
 
 #import sys
 #sys.path.append('/home/pinsley/miniconda2/pkgs/bokeh-0.12.4-py27_0/bin')
@@ -27,18 +27,18 @@ if __name__ == '__main__':
 def handle_data():
 #    if request.method == 'POST':
     test = request.values.get('input_ticker')
-    fig = figure()
-    fig.line([1,2,3,4],[5,6,7,8])
-    js_resources = INLINE.render_js()
-    css_resources = INLINE.render_css()
+#    fig = figure()
+#    fig.line([1,2,3,4],[5,6,7,8])
+#    js_resources = INLINE.render_js()
+#    css_resources = INLINE.render_css()
 
-    html = flask.render_template(
-        'embed.html',
-        plot_script=script,
-        plot_div=div,
-        js_resources=js_resources,
-        css_resources=css_resources,
-    )
+#    html = flask.render_template(
+#        'embed.html',
+#        plot_script=script,
+#        plot_div=div,
+#        js_resources=js_resources,
+#        css_resources=css_resources,
+#    )
 
     return encode_utf8(html)
 
